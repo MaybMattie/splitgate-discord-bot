af20254e276d8ebe780d1af744085a73b344f3b9
Mattie Headley <mrmattieh@gmail.com>
2021-08-26 19:19:12 -0400

    tyler monkey

--- monkey.js
+++ monkey.js
@@ -1,10 +1,15 @@
 const Discord = require('discord.js')
+const tylerID = '627300050673991680'
 
 module.exports = (client) => {
     client.on('message', (message) => {
         const { channel, member, content, guild } = message
         if (member.id === client.user.id) return
-        if (content.includes('among') && content.includes('us')) {
+        if (member.id === tylerID) {
+            message.reply('https://tenor.com/view/monkey-monki-crazy-calm-gif-18390175')
+            return
+        }
+        if (content.toLowerCase().includes('among') && content.toLowerCase().includes('us')) {
             channel.send(`banning ${member} for talking about among us`)
             let nickname = member.nickname
             member.setNickname('AMONG US BAD')
