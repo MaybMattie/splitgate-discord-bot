354d64bf6737a6a82025af16cae84753722b0eed
Mattie Headley <mrmattieh@gmail.com>
2021-08-26 18:06:32 -0400

    PUTTING IT DOWN

--- monkey.js
+++ monkey.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
     client.on('message', (message) => {
         const { channel, member, content, guild } = message
         if (content.includes('among') && content.includes('us')) {
-            channel.send(`banning ${member} for talking about among us`)
+            // channel.send(`banning ${member} for talking about among us`)
         }
     })
 }
