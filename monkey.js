2d02ec564877d8528d2c2f944b49f182e23c658c
Mattie Headley <mrmattieh@gmail.com>
2021-08-26 18:07:17 -0400

    checks user

--- monkey.js
+++ monkey.js
@@ -3,8 +3,9 @@ const Discord = require('discord.js')
 module.exports = (client) => {
     client.on('message', (message) => {
         const { channel, member, content, guild } = message
+        if (member.id === client.member.id) return
         if (content.includes('among') && content.includes('us')) {
-            // channel.send(`banning ${member} for talking about among us`)
+            channel.send(`banning ${member} for talking about among us`)
         }
     })
 }
